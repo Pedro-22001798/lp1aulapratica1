7e0212b14057beb0c770a2d671fa703efd5bb0e4
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 12:05:49 +0000

    Finished strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -15,5 +15,18 @@
             Console.WriteLine(three);
             Console.WriteLine(four);
             Console.WriteLine(five);
+
+            string six = "a" + 2;
+            int x = 500;
+            int y = 100;
+            string seven = $"valor de x é {x}";
+            string eight = $"valor de x + y = {x+y}";
+            string nine = String.Format("Y é {1}, X é {0}", x,y);
+
+            Console.WriteLine(six);
+            Console.WriteLine(seven);
+            Console.WriteLine(eight);
+            Console.WriteLine(nine);
+            Console.WriteLine(@"Verbatim com x = {0}",x);
         }
     }
