03d23190fb265e4273acc87c9f08d091ef9f3a9d
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 11:30:32 +0000

    OlaMundo

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+        }
+    }
