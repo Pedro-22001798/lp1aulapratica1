d8d331df0b6113b2e95d7e5e4008bc2e56a9bad8
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 11:19:24 +0000

    First commit

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            foreach(string s in stringNums)
+            {
+                int num;
+                bool checkInt = int.TryParse(s, out num);
+
+                if(checkInt)
+                {
+                    string numHex = num.ToString("X");
+                    Console.WriteLine($"{num} -> é um número inteiro. Hexa = {numHex}.");
+                }
+                else
+                {
+                    Console.WriteLine($"{s} -> não é um número inteiro.");
+                }
+            }
+        }
+    }
