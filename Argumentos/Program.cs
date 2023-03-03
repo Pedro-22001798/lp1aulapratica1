e2d45da62c71aa2e12b1fa230086fa82b67db5eb
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 11:27:02 +0000

    Argumentos done

---
+++ Argumentos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            foreach(string s in stringNums)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
