d836427a89a6af144f92b5fae7ad32e7f3bc387b
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 11:42:40 +0000

    TiposVarios Interios

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            sbyte one = -128;
+            short two = -32768;
+            int three = 50000;
+            long four = 4611686018427387904;
+            byte five = 254;
+            ushort six = 65534;
+            uint seven = 4294967295;
+            ulong eight = 18446744073709551615;
+            char nine = '\u00A9';
+
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+            Console.WriteLine(three);
+            Console.WriteLine(four);
+            Console.WriteLine(five);
+            Console.WriteLine(six);
+            Console.WriteLine(seven);
+            Console.WriteLine(eight);
+            Console.WriteLine(nine);
+        }
+    }
