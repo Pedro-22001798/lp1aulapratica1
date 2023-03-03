c0b2555f6f2f245f733efee1ec5bc4fd04435de1
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-03 12:00:16 +0000

    String types

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            string one = "simples";
+            string two = "\n";
+            string three = @"Não é um codigo Unicode \u1234, não é' nova linha \n";
+            string four = @"""Esta string esta entre aspas""";
+            string five = "Esta string não está entre aspas";
+
+            Console.WriteLine(two);
+            Console.WriteLine(one);
+            Console.WriteLine(three);
+            Console.WriteLine(four);
+            Console.WriteLine(five);
+        }
+    }
