49abb4ce5309bd09c56085ee280706c6d3bafd5e
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-10 10:48:23 +0000

    PerguntaValores finished

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Qual é a altura? ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual é o raio? ");
+            float r = float.Parse(Console.ReadLine());
+
+            double volume = 3.1415926 * (r * r) * a;
+            double area = 2 * 3.1415926 * (r * r + r * a);
+            Console.WriteLine($"Volume é {volume}");
+            Console.WriteLine($"Area é {area}");
+        }
+    }
