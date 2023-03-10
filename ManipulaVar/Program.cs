a880e9d6a53a9e375c5594b67015094285c703b7
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-10 10:37:00 +0000

    ManipulaVar

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            int x = int.Parse(stringNums[0]);
+            x++;
+            Console.WriteLine(x);
+        }
+    }
