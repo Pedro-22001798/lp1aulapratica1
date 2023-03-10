fb7a5fc10aae6daec8c8d9d5e44b717b4970abbf
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-10 11:25:20 +0000

    ChatGPTMother

--- Semana2/ChatGPTMother/Program.cs
+++ Semana2/ChatGPTMother/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string question = Console.ReadLine();
+            while(question != "EXIT")
+            {
+                string response = question switch
+                {
+                    "What's your name?" => "My name is ChatGPTMother.",
+                    "Who is your son?" => "My son is ChatGPT",
+                    "Do you work for the FBI?" => "Cmon you know I am...",
+                    _ => "I don't recognize that question, sorry :(",
+                };
+                Console.WriteLine(response);
+                question = string.Empty;
+                question = Console.ReadLine();
+            }
+        }
+    }
