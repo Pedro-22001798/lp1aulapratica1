0f8b922b74b8504cefa9a2f36c294ff080558f52
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-10 12:00:39 +0000

    Diamond created

---
+++ Semana2/Diamond/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            int quantity = int.Parse(stringNums[0]);
+            if(quantity % 2 == 0)
+            {
+                Console.WriteLine("I only want odd numbers!");
+            }
+            else
+            {
+
+            }
+        }
+    }
